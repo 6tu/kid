73aa19afd89e79cddc8b339f54e175f3e949b773
六先生 <50118271+six-mr@users.noreply.github.com>
2020-06-16 02:59:10 +0800

    Add files via upload

--- mmh/mmh-proxy.php
+++ mmh/mmh-proxy.php
@@ -1,41 +1,129 @@
 <?php
-
 // highlight_file("caiji-mmh.php");
 ignore_user_abort();
 set_time_limit(0);
 error_reporting(1);
 date_default_timezone_set('America/New_York');
+// echo date("Y-n-j", time());
+
+# $time 是文件发布日期，$time0 是系统当前日期
+
+# 上传到对象服务器
+$up2qiniu = false;                     # true为发送,false为不发送
+
+$host = 'http://m.minghui.org';
+$url_base = $host . '/mh/articles/';
 
 $loc = $_SERVER['REQUEST_SCHEME'] .'://'. $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) .'/';
-
 
 $mhdata = 'mhdata/';
 $cwd = getcwd();
 if(!is_dir($cwd.'/'.$mhdata)) mkdir($cwd.'/'.$mhdata, 0777, true);
 
-$time = date("Y-n-j", time());
-if(file_exists($mhdata . $time. '.zip.p7m')) die($loc . $mhdata . $time. '.zip.p7m');
-$headers = get_headers('https://shideyun.com/mmh/mhdata/' . $time . '-t.zip.sha512');
-if(!strpos($headers[0], '200')) echo("error,今日尚未发布");
+# 用浏览器访问
+if(empty($_GET['mhdaily']) and !strstr($_SERVER['HTTP_USER_AGENT'], 'Wget')){
+    form_html();
+    exit(0);
+}
 
-$host = 'https://mmh.6tu.me';
-$url = $host . '/mmh/index.html';
+ob_end_flush();//关闭缓存
+
+echo "<br>正在检测远程文件 <br>\r\n";
+
+# 由GET变量传递的文件名和URL
+if(isset($_GET['mhdaily'])){
+    $fn = $_GET['mhdaily'];
+    $regex="'\d{4}-\d{1,2}-\d{1,2}-t.zip'is";
+    preg_match_all($regex,$fn,$matches);
+    // print_r($matches[0]);
+    if(empty($matches[0])) die("文件名格式不匹配");
+
+    $time = substr($fn, 0, -6); #文件名除去后缀
+    if(!strtotime($time)) die("日期格式不匹配");
+    $year =  date("Y", strtotime($time));
+    $month = date("n", strtotime($time));
+    $day =   date("j", strtotime($time));
+    $monthday = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+    
+    $url_path_date = $year .'/'. $month .'/'. $day .'/';
+    $fn_date_zip = $time .'-t.zip';
+    $fn_date_md5 = $time .'.md5';
+    $url_t_zip = $url_base . $url_path_date . $fn_date_zip;
+}else{
+    $time = date("Y-n-j", time());
+    $year =  date("Y", time());
+    $month = date("n", time());
+    $day =   date("j", time());
+    $monthday = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+
+    $url_path_date = $year .'/'. $month .'/'. $day .'/';
+    $fn_date_zip = $year .'-'. $month .'-'. $day .'-t.zip';
+    $fn_date_md5 = $year .'-'. $month .'-'. $day .'.md5';
+    $url_t_zip = $url_base . $url_path_date . $fn_date_zip;
+}
+
+
+# md5 控制文件是否更新
+$time0 = time(); # 当前日期
+if(file_exists($mhdata . $fn_date_md5)){
+    $old_md5 = file_get_contents($mhdata . $fn_date_md5);
+    $md5_time = filemtime($mhdata . $fn_date_md5);
+    $time_diff = round(($time0 - $md5_time)/60, 1);
+    if(($time0 - $md5_time) < 3600) die("$time_diff 更新频繁 . <br>\r\n");;
+}else $old_md5 = '';
+
+$url_headers = get_headers($url_t_zip);
+if(!strpos($url_headers[0], '200')) die("未找到或者尚未发布 . <br>\r\n");
+$zip_tmp = get_file_progress($url_t_zip);
+file_put_contents($mhdata . $fn_date_zip, $zip_tmp);
+$last_md5 = md5_file($mhdata . $fn_date_zip);
+// unlink($fn_date_zip);
+if($last_md5 === $old_md5){
+    file_put_contents($mhdata . $fn_date_md5, $last_md5);
+    die("无需更新 . <br>\r\n");
+}else{
+    file_put_contents($mhdata . $fn_date_md5, $last_md5);
+    // echo "";
+}
+
+
+
+
+
+$file_gz = substr(md5($time), 8, 16) . '.gz';
+
+if(file_exists($mhdata . $file_gz) and ($time0 - filemtime($mhdata . $file_gz)) < 3600){
+    die($loc . $mhdata . $file_gz);
+}
+
+$url = $host . '/mmh/articles/' .$url_path_date. 'index.html';
+
+$path_fn = mkpath($url);
 $res_array = getResponse($url, $data = [], $cookie_file = '');
 $html = $res_array['body'];
-$array_url = getlink($html);
-$path_fn = mkpath($url);
 file_put_contents($path_fn, $html);
 unset($res_array);
 
+$array_url = getlink($html);
+
+echo "\r\n<br><br>正在离线下载 <br><pre>\r\n";
+
 $img_url = array();
 foreach($array_url as $url){
-    $url = parse_url($url, PHP_URL_PATH);
-    
-    $url = $host . $url;
+    if(empty($url)) continue;
+    if(empty(parse_url($url, PHP_URL_SCHEME)) or empty(parse_url($url, PHP_URL_HOST))){
+        $url = parse_url($url, PHP_URL_PATH);
+        $url = $host . $url;
+    }else echo $url . "<br>\r\n";
+    $url = trim($url);
     $path_fn = mkpath($url);
     $res_array = getResponse($url, $data = [], $cookie_file = '');
     $html = $res_array['body'];
     file_put_contents($path_fn, $html);
+++$i;
+echo '.';
+flush();
+clearstatcache();
     unset($res_array);
     if(strpos($url, '.html') !== false and strpos($url, '/mh/articles/') !== false){
         $daily = $url;
@@ -48,32 +136,53 @@ foreach($array_url as $url){
     }
     unset($array_article_url);
 }
+
+//print_r($array_url); print_r($img_url);
+$all_url = array_unique(array_merge($img_url, $array_url));
+// file_put_contents('url.log', print_r($all_url, true));
+echo " $i <br>\r\n";
+
 foreach($img_url as $url){
-    $url = parse_url($url, PHP_URL_PATH);
-    $url = $host . $url;
+    if(empty($url)) continue;
+    if(empty(parse_url($url, PHP_URL_SCHEME)) or empty(parse_url($url, PHP_URL_HOST))){
+        $url = parse_url($url, PHP_URL_PATH);
+        $url = $host . $url;
+    }else echo "\r\n<li>" . $url . "</li>\r\n";
+    $url = trim($url);
     $path_fn = mkpath($url);
     $res_array = getResponse($url, $data = [], $cookie_file = '');
     $html = $res_array['body'];
     file_put_contents($path_fn, $html);
+++$ii;
+echo '.';
+flush();
 }
+echo " $ii <br>\r\n";
 
-echo "<h4>相关文件下载完毕</h4> <pre>\r\n";
-echo "<b>$daily </b>\r\n\r\n";
-//print_r($array_url);
-//print_r($img_url);
-$all_url = array_unique(array_merge($img_url, $array_url));
-$zipfn = substr(strrchr($daily, '/'), 1, -4) . 'zip';
-$txtfn = substr(strrchr($daily, '/'), 1, -4) . 'txt';
-// file_put_contents($txtfn, print_r($all_url, true));
+$n = $i + $ii;
+$index_fn = basename($daily);
+echo "<b>$index_fn 中 $n 个文件下载完毕</b>  \r\n\r\n";
+
+sleep(2);
+
+// $basename = substr(strrchr($daily, '/'), 1, -5);
+// $td = strstr(basename($fn), '.', true); # 文件名中提取日期
+// $ts = strtotime($td);                  # 时间截
+
+$basename = strstr(basename($daily), '.', true);
+$zipfn = $basename . '.zip';
+$txtfn = $basename . '.txt';
+$file_p7m = $zipfn . '.p7m';
+
 if(file_exists($zipfn)) unlink($zipfn);
 $all_url[] .= '/pub/mobile.css';
 $all_url[] .= '/pub/favicon.ico';
 foreach($all_url as $fn){
     $fn = str_replace($host . '/', '', $fn);
     $fn = ltrim($fn, '/');
-    compress($fn, $zipfn);
+    zip_file($fn, $zipfn);
 }
-echo "<b>$zipfn 打包完成 </b>\r\n\r\n";
+echo "$zipfn 打包完成 <br>\r\n";
 
 foreach($all_url as $fn){
     $fn = str_replace($host . '/', '', $fn);
@@ -84,17 +193,25 @@ foreach($all_url as $fn){
 delDirAndFile('mmh');
 delDirAndFile('mh');
 
-$smime = pkcs7_encrypt($zipfn);
-$file_p7m = $zipfn . '.p7m';
-echo $smime . $loc_zip = $loc . $mhdata . $file_p7m;
-rename($file_p7m, $mhdata . $file_p7m);
+echo pkcs7_encrypt($zipfn);
+sleep(1);
+$file_md5_gz = substr(md5($basename),8,16) . '.gz';
+$file_md5_zip = substr(md5($basename),8,16) . '.zip';
+if(extension_Loaded('zlib')){
+    file_put_contents($mhdata . $file_md5_gz, gzencode( file_get_contents($file_p7m), 9));
+}
+echo zip_file($file_p7m, $mhdata . $file_md5_zip);
+
+echo "压缩包地址 " . $loc . $mhdata . $file_md5_gz;
+
+// rename($file_p7m, $mhdata . $file_p7m);
+
 unlink($zipfn);
 unlink($file_p7m);
+if($up2qiniu) upload2qiniu(getcwd() .'/'. $mhdata . $file_md5_gz);
 
 
-
-
-
+echo "</pre><br>\r\n";
 
 
 
@@ -113,6 +230,46 @@ unlink($file_p7m);
 
 
 /** =========函数区========= */
+
+function form_html(){
+    $time = date("Y-n-j", time());
+    $fn = $time . '-t.zip';
+    $html = "<body><br><center>\r\n";
+    $html .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="GET" />' . "\r\n";
+    $html .= "  <b>?mhdaily= </b>\r\n";
+    $html .= '  <input type="text" name="mhdaily" size=20 value="'.$fn.'" />'."\r\n";
+    $html .= '  <input type="submit" value="Send" />'."\r\n";
+    $html .= "</form>\r\n</center></body>";
+    echo $html;
+}
+
+# 上传文件到七牛云对象存储服务器
+function upload2qiniu($filePath, $key){
+    $qiniupara = array(
+        'qiniupath' => __DIR__ . '/qiniu/upload_to_qiniu.php',
+        'qiniuurl' => 'http://80luir.s3-cn-south-1.qiniucs.com/',
+        'accessKey' => 'KzBWtGa-Qsxd2zA_SbYkcxi9Evw0fRNgQY5ax9T6',
+        'secretKey' => 'F8JQ4riqVfQmgCyDWya9Oi5TYBtNpOKBToYUxEyh',
+        'bucket' => 'statics',
+        );
+
+    $reqpath   = $qiniupara['qiniupath'];
+    $puburl    = $qiniupara['qiniuurl'];
+    $accessKey = $qiniupara['accessKey'];
+    $secretKey = $qiniupara['secretKey'];
+    $bucket    = $qiniupara['bucket'];
+
+    require_once $reqpath;
+    // $filePath                              #本地文件
+    // $key = $p7m_zip;                       #上传到七牛后保存的文件名
+
+    if($err !== null){
+        var_dump($err);
+    }else{
+        // var_dump($ret);
+        echo 'Successfully uploaded to <a href="' . $puburl . $ret['key'] . '">qiniu</a>';
+    }
+}
 
 # 遍历当前目录
 // print_r(listDir('./'));
@@ -154,6 +311,65 @@ function mkpath($url){
     if(!is_dir($path))mkdir($path, 0777, true);
     return $path . '/' . $fn;
 }
+# 获取网页中超链接的两种方法
+function preg_htmllink($html){
+    $html = preg_replace('/\s{2,}|\n/i', '', $html); # 过滤掉换行和2个以上的空格
+    preg_match_all('/(?:img|a|source|link|script)[^>]*(?:href|src)=[\'"]?([^>\'"\s]*)[\'"]?[^>]*>/i', $html, $out);
+    return($out[1]);
+}
+
+# 获取网页中超链接
+# PHP DOM XPath获取HTML节点方法大全
+# https://www.awaimai.com/2113.html
+function dom_htmllink($html){
+    $dom = new DOMDocument();
+    @$dom -> loadHTML($html);
+    $xpath = new DOMXPath($dom);
+    
+    # 获取 css 链接
+    $nodeList = $xpath -> query("//link");
+    $css = [];
+    foreach ($nodeList as $node){
+        $css[] = $node -> attributes -> getNamedItem('href') -> nodeValue;
+    }
+    # 获取 js 链接
+    $nodeList = $xpath -> query("//script");
+    $js = [];
+    foreach ($nodeList as $node){
+        $js[] = @$node -> attributes -> getNamedItem('src') -> nodeValue;
+    }
+    # 获取 mp4 链接
+    $nodeList = $xpath -> query("//source");
+    $mp4 = [];
+    foreach ($nodeList as $node){
+        $mp4[] = $node -> attributes -> getNamedItem('src') -> nodeValue;
+    }
+    # 获取 img 链接
+    $nodeList = $xpath -> query("//img");
+    $img = [];
+    foreach ($nodeList as $node){
+        $img[] = $node -> attributes -> getNamedItem('src') -> nodeValue;
+    }
+    # 获取 htm 链接
+    $nodeList = $xpath -> query("/html/body//a");
+    // for ($i = 0; $i < $hrefs -> length; $i++) {
+    //     $href = $hrefs -> item($i);
+    //     $url = $href -> getAttribute('href');
+    //     echo $url . "\r\n";
+    // }
+    $htm = [];
+    foreach ($nodeList as $node){
+        $htm[] = $node -> attributes -> getNamedItem('href') -> nodeValue;
+    }
+    
+    $link = array_merge($css, $js, $mp4, $img, $htm);
+    shuffle($link);
+    foreach ($link as $key => $val){
+        if (empty($val)) continue;
+        $links[] = $val;
+    }
+    print_r($links);
+}
 
 function getlink($html){
     $array_url = array();
@@ -178,7 +394,7 @@ function unzip_file($file, $destination){
     return " Archive extracted to directory <br><br>\r\n";
 }
 
-function compress($txtname, $zipname){
+function zip_file($txtname, $zipname){
     if(false !== function_exists("zip_open")){
         $zip = new ZipArchive();
         if($zip -> open($zipname, ZIPARCHIVE :: CREATE) !== TRUE){
@@ -192,7 +408,7 @@ function compress($txtname, $zipname){
         $test -> add_files(array($txtname));
         $test -> create_archive();
     }
-    return " Create a compressed file $zipname success <br>\r\n";
+    return ".zip successfully created<br>\r\n";
 }
 
 #  [, 表示之后为可选
@@ -240,7 +456,7 @@ x7W+ZnmjO8yJXTdYfwmQxv2SOulDZtoe
     if($path_type == 1) $infile = $cwd . '/' . $infile;
     $outfile_p7m = $infile . '.p7m';
     if(openssl_pkcs7_encrypt($infile, $outfile_p7m, $cert, $headers, PKCS7_BINARY)){
-        return "smime.p7m successfully created <br>\r\n";
+        return ".p7m successfully created <br>\r\n";
     }else return "Encryption failed <br>\r\n";
     # unlink($enc);
 }
@@ -351,9 +567,9 @@ function getResponse($url, $data = [], $cookie_file = ''){
     $headers = explode("\r\n", $res_array[0]);
     $status = explode(' ', $headers[0]);
     # 如果$headers为空，则连接超时
-    if(empty($res_array[0])) die('<br><br><center><b>连接超时</b></center>');
+    if(empty($res_array[0])) echo("<br>'.$url.'<b> 连接超时</b><br>\r\n");
     # 如果$headers状态码为404，则自定义输出页面。
-    if($status[1] == '404') die("<pre><b>找不到，The requested URL was not found on this server.</b>\r\n\r\n$res_array[0]</pre>\r\n\r\n");
+    if($status[1] == '404') echo($url."<pre><b>找不到，The requested URL was not found on this server.</b>\r\n\r\n$res_array[0]</pre>\r\n\r\n");
     # 如果$headers第一行没有200，则连接异常。
     # if($status[1] !== '200') die("<pre><b>连接异常，状态码： $status[1]</b>\r\n\r\n$res_array[0]</pre>\r\n\r\n");\
 
@@ -411,11 +627,14 @@ function getResponse($url, $data = [], $cookie_file = ''){
     return $res_array;
 }
 
-
 function get_html($url){
     $path_parts = pathinfo($url);
     $refer = $path_parts['dirname'] . '/' . $_SERVER['PHP_SELF'];
-    $option = array('http' => array('header' => "Referer:$refer"), 
+    $option = array('http' => array(
+                            'header' => "Referer: $refer",
+                            'method' => "GET",
+                            'timeout' => 10,
+                            ), 
                     'ssl' => array('verify_peer' => false, 'verify_peer_name' => false,),
                     );
     $html = @file_get_contents($url, false, stream_context_create($option));
@@ -425,3 +644,54 @@ function get_html($url){
         return $html;
     }
 }
+
+function progress($resource, $dl_size, $dl, $up_size, $up){
+    # 最好是开启session，让其记忆次数 
+    $tmp = 'tmp.log';
+    if(!defined('tmp_log')){
+        $randstr = '';
+        for($i = 0;$i < 5;$i++){
+            $randstr .= chr(rand(97, 122));
+        }
+        $tmp = str_shuffle($randstr . rand(100, 999));
+        $num = rand(100, 999) . '.log';
+        @define("tmp_log", $tmp . '.log');
+    }
+    $tmp = tmp_log;
+
+    if($dl_size > 0){
+        if(!file_exists($tmp)){
+            $i = 0;
+            file_put_contents($tmp, 1);
+        }else{
+            $i = file_get_contents($tmp);
+            $i = intval($i) + 1;
+            file_put_contents($tmp, $i);
+        }
+        $decimal = $dl / $dl_size * 100;
+        $decimal = round($decimal, 2);
+        $per = $decimal . "%";
+        echo '.';
+        if(is_int($i/100) and ($i/100) > 0) echo " $per\r\n";
+        if($decimal > 99.98) file_put_contents($tmp, '');
+    }
+    ob_flush();
+    flush();
+    //sleep(1);
+}
+
+function get_file_progress($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    //curl_setopt($ch, CURLOPT_BUFFERSIZE,128);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, 'progress');
+    curl_setopt($ch, CURLOPT_NOPROGRESS, false); //false表示用进度条
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    $html = curl_exec($ch);
+    curl_close($ch);
+    return $html;
+}
+
